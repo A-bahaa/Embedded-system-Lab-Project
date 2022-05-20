45428e8109e417580a9c4b7f02d630f367e40af1
MahmoudKamal01 <53492856+MahmoudKamal01@users.noreply.github.com>
2022-05-20 03:00:53 +0200

    Update SpaceInvaders.c

--- Project-Files/SpaceInvaders.c
+++ Project-Files/SpaceInvaders.c
@@ -3,20 +3,31 @@
 
 void Timer2_Init(unsigned long period);
 void Delay100ms(unsigned long count);
-
+void print_(int score);
 int main(void){
+	
     PortF_Init();
     EdgeCounter_Init();           // initialize GPIO Port F interrupt
     TExaS_Init(SSI0_Real_Nokia5110_Scope);  // set system clock to 80 MHz
     Random_Init(1);
+    ADC_Init();
     Nokia5110_Init();
-		v1 = CARH, v2 =  CARH - 24 ;
-		h = 32;
-		r1 = 1;
-		r2 = 3;
-		x = 0; 
+    vertical_pos1 = CARH, vertical_pos2 =  CARH - 24 ;
+    horizontal_pos = 32;
+    r1 = 1;
+    r2 = 3;
+    score = 0; 
 	
   while(1){
+		
+		if((ADC0_RIS_R & (1 << 3)) == (1 << 3)){ // if third bit is set
+			ADC0_ISC_R  |= (1<<3);  // third bit = 1
+			addData = ADC0_SSFIFO3_R;
+		}
+		horizontal_pos = (addData * (80-CARSW))/4095;
+		
+		
+		
 		if(v1 > 48) x ++,r1 = rand ()%31, v1 = 0;  // for new random number and restart
 		if(v2 > 48) x ++,r2 = rand ()%31, v2 = 0;  // for new random number and restart
 			Enemies(h,v1,v2,r1,r2);
